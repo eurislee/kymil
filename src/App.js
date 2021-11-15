21921968f2af5c12efaa4e4dea4c7d4de7451c4f
Euris Lee <eurislee@gmail.com>
2021-11-15 08:05:38 -0800

    finished lesson 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
+
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <ContactList contacts={[
+            { name: "Michael" },
+            { name: "Ryan" },
+            { name: "Tyler" }
+        ]}/>
+        <ContactList contacts={[
+            { name: "Euris" },
+            { name: "Lee" }
+
+        ]}/>
       </div>
     );
   }
